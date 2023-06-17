4743b82a59e8a6332863c1f3d85079cc74fe0f95
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-06-17 10:10:49 -0700

    Update Ordered-count-of-characters.js

--- Ordered-count-of-characters.js
+++ Ordered-count-of-characters.js
@@ -11,7 +11,8 @@ orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]
 */
 
 
+// Solution
+
 const orderedCount = s =>
   Array.from(s.split('').reduce((m, k) => m.set(k, m.has(k) ? m.get(k) + 1 : 1), new Map()))
 
-  
