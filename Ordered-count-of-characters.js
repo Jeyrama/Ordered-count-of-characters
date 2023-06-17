2ebf0eae8c15f0af16db566259fd2ee0d5d771dc
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-06-17 10:18:41 -0700

    Update Ordered-count-of-characters.js

--- Ordered-count-of-characters.js
+++ Ordered-count-of-characters.js
@@ -18,3 +18,19 @@ const orderedCount = s =>
 
 // or
 
+let orderedCountOfCharacters = function (text) {
+  let arr=[];
+  for (let i = 0; i < text.length; ++i) {
+    let k = 0;
+    for (let j = 0; j < arr.length; ++j)
+      if (arr[j][0]==text[i])
+        k++;
+    if (k==0) {
+      for (let j = i; j < text.length; ++j)
+        if (text[j]==text[i])
+          k++;
+        arr.push([text[i],k]);
+    }
+  }
+  return arr;
+}
